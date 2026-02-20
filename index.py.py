b99c1fa98f5b4e3bcf8a03c18fa66f3c4bb6f35b
Sejal Gupta <160370932+sejz25@users.noreply.github.com>
2026-02-20 11:14:24 +0530

    Add files via upload

---
+++ index.py.py
@@ -0,0 +1,9 @@
+# Step 1: Take input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Step 2: Add the two numbers
+sum = num1 + num2
+
+# Step 3: Display the result
+print("Sum of the two numbers is:", sum)
